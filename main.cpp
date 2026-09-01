16b93bc9d63258582165c6c48126bdc3141250f7
YUJIN <elly3385@gmail.com>
2026-09-01 14:56:17 +0900

    feat: 역과 구간 연결 기능 구현

--- main.cpp
+++ main.cpp
@@ -20,6 +20,44 @@ struct Edge
 vector<Node> nodes;
 vector<vector<Edge>> graph;
 
+int findNode(string station, int line)
+{
+    for (int i = 0; i < nodes.size(); i++)
+    {
+        if (nodes[i].station == station && nodes[i].line == line)
+        {
+            return i;
+        }
+    }
+
+    nodes.push_back({ station, line });
+    graph.push_back({});
+
+    return nodes.size() - 1;
+}
+
+void addEdge(string a, string b, int line, int time)
+{
+    int aIndex = findNode(a, line);
+    int bIndex = findNode(b, line);
+
+    graph[aIndex].push_back({ bIndex, time });
+    graph[bIndex].push_back({ aIndex, time });
+}
+
+void addLine(int line, vector<string> stations, vector<int> times)
+{
+    for (int i = 0; i < times.size(); i++)
+    {
+        addEdge(
+            stations[i],
+            stations[i + 1],
+            line,
+            times[i]
+        );
+    }
+}
+
 int main()
 {
     return 0;
